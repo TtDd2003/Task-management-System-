03acd3e14ffe403709e96b3178b0a89d1e15b297
Trinita Das <trinitadas76@gmail.com>
2025-11-02 21:51:28 +0530

    conn.php

--- Task_manager/conn.php
+++ Task_manager/conn.php
@@ -12,4 +12,18 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+//in profree host --- 
+//$servername = "sql109.ezyro.com";    From your ProFreeHost MySQL section
+//$username = "ezyro_40229821";
+//$password = "T@skM@nager2025!";
+//$dbname = "ezyro_40229821_task_manager";
+
+//$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//if (!$conn) {
+//    die("<h3 style='color:red;'>❌ Connection failed: " . mysqli_connect_error() . "</h3>");
+//}
+//echo "<h3 style='color:green;'>✅ Connected successfully to the database!</h3>";
 ?>
+
